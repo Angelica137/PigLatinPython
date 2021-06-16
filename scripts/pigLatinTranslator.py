4062ef597c90c92ac2024552f5c53100cfeabdc9
Angelica <Angelica137@users.noreply.github.com>
2021-06-16 22:13:59 +0100

    xray returns xrayay

--- scripts/pigLatinTranslator.py
+++ scripts/pigLatinTranslator.py
@@ -8,3 +8,13 @@ class PigLatinTranslator:
     def translate(self):
         if self.word[0] == "a" or self.word[0] == "e" or self.word[0] == "i" or self.word[0] == "o" or self.word[0] == "u":
             return self.word + "ay"
+        elif self.word[:2] == "xr":
+            return self.word + "ay"
+
+
+pig1 = PigLatinTranslator("eye")
+print(pig1.translate())
+
+
+pig2 = PigLatinTranslator("xray")
+print(pig2.translate())
