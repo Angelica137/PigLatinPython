efb806510780ad4d10a070b54fe9b598ce896326
Angelica <Angelica137@users.noreply.github.com>
2021-06-16 22:00:11 +0100

    cray returns xrayay

--- tests/test_pigLatinTranslator.py
+++ tests/test_pigLatinTranslator.py
@@ -4,3 +4,8 @@ from scripts.pigLatinTranslator import PigLatinTranslator
 def test_word_starts_w_vowel_ads_ay_at_end():
     pig = PigLatinTranslator("eye")
     assert pig.translate() == "eyeay"
+
+
+def test_word_starts_w_xr_returns_xrayay():
+    pig = PigLatinTranslator("xray")
+    assert pig.translate() == "xrayay"
