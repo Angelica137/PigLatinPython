001473d3e964e08ce69173a0e6afd12860be53cd
Angelica <Angelica137@users.noreply.github.com>
2021-06-19 23:44:33 +0100

    chair returns airchay, green

--- scripts/pigLatinTranslator.py
+++ scripts/pigLatinTranslator.py
@@ -6,13 +6,26 @@ class PigLatinTranslator:
         return self.word
 
     def translate(self):
-        if self.word[0] == "a" or self.word[0] == "e" or self.word[0] == "i" or self.word[0] == "o" or self.word[0] == "u":
-            return self.word + "ay"
-        elif self.word[:2] == "xr":
-            return self.word + "ay"
-        elif self.word[:2] == "yt":
-            return self.word + "ay"
+        consonants = ""
+        translation = ""
+        for char in range(0, len(self.word)):
+            if self.word[char] == "a" or self.word[char] == "e" or self.word[char] == "i" or self.word[char] == "o" or self.word[char] == "u":
+                break
+            elif self.word[:2] == "xr":
+                return self.word + "ay"
+            elif self.word[:2] == "yt":
+                return self.word + "ay"
+            else:
+                consonants += self.word[char]
+
+        # format translation
+        translation = self.word[char:len(self.word)]
+        return translation + consonants + "ay"
 
 
-pig1 = PigLatinTranslator("yttria")
+pig1 = PigLatinTranslator("chair")
+print(pig1.translate())
+
+
+pig1 = PigLatinTranslator("eye")
 print(pig1.translate())
